8708239297076fd5307ec18201faaaf6a30dd60c
Romulo Vieira <romulo.vieira777@gmail.com>
2024-09-27 09:34:51 -0300

    Arquitetura do SQL Server - Introdução

--- Secao_05_Arquitetura_do_SQL_Server_Introducao/SQL_Code/04_Select_com_Tabelas.sql
+++ Secao_05_Arquitetura_do_SQL_Server_Introducao/SQL_Code/04_Select_com_Tabelas.sql
@@ -56,3 +56,197 @@ SELECT
 FROM
    [RH].[Empregado]
 GO
+
+
+SELECT
+	*
+FROM
+	[FundamentosTSQL].[Rh].[Empregado]
+GO
+
+
+-- Organizando o código para uma melhor leitura.
+SELECT
+	RazaoSocial AS Empresa
+  , Endereco    AS Local
+  , CEP			AS CodigoPostal
+FROM
+	[FundamentosTSQL].[Vendas].[Cliente]
+GO
+
+
+-- Filtrando linhas.
+
+-- Apresentando apenas um cliente
+SELECT
+	*
+FROM
+	[FundamentosTSQL].[Vendas].[Cliente]
+WHERE
+	iIDCliente = 1
+GO
+
+
+SELECT
+	*
+FROM
+	[FundamentosTSQL].[Vendas].[Cliente]
+WHERE
+	Pais = 'Brazil'
+GO
+
+
+-- Mostrando os empregados com salario menor que 50.000 anual
+SELECT
+	*
+FROM
+	[FundamentosTSQL].[Rh].[Empregado]
+WHERE
+	Salario < 50000
+GO
+
+
+-- Apresentando os clientes do pais Brazil e do estado do Rio de Janeiro
+SELECT
+    *
+FROM
+    [FundamentosTSQL].[Vendas].[Cliente]
+WHERE
+    Pais = 'Brazil'
+AND
+	Cidade = 'Rio de Janeiro'
+GO
+
+
+-- Mostrar todos os pedidos solicitados em 2006!
+SELECT
+    *
+FROM
+    [FundamentosTSQL].[Vendas].[Pedido]
+WHERE
+    DataPedido >= '2006-01-01'
+AND
+    DataPedido <= '2007-01-01'
+GO
+
+
+-- Mostrar pedidos enviados para o Rio de Janeiro e São Paulo
+SELECT
+    *
+FROM
+    [FundamentosTSQL].[Vendas].[Pedido]
+WHERE
+    shipCidade = 'Rio de Janeiro'
+OR
+    shipCidade = 'São Paulo'
+GO
+
+
+-- Ordenar os resultados
+SELECT
+    RazaoSocial
+  , Contato
+  , Pais
+  , Cidade
+FROM
+    [FundamentosTSQL].[Vendas].[Cliente]
+ORDER BY
+    Pais
+GO
+
+
+SELECT
+    RazaoSocial
+  , Contato
+  , Pais
+  , Cidade
+FROM
+    [FundamentosTSQL].[Vendas].[Cliente]
+ORDER BY
+    Pais
+  , Cidade
+GO
+
+
+SELECT
+    PrimeiroNome + ' ' + UltimoNome AS Nome
+  , Cargo
+  , DataAdmissao
+FROM
+    [FundamentosTSQL].[Rh].[Empregado]
+ORDER BY
+    PrimeiroNome
+GO
+
+
+SELECT
+    PrimeiroNome + ' ' + UltimoNome AS Nome
+  , Cargo
+  , DataAdmissao
+FROM
+    [FundamentosTSQL].[Rh].[Empregado]
+ORDER BY
+    Nome
+GO
+
+
+SELECT
+    PrimeiroNome + ' ' + UltimoNome AS Nome
+  , Cargo
+  , DataAdmissao
+FROM
+    [FundamentosTSQL].[Rh].[Empregado]
+ORDER BY
+    1
+GO
+
+
+-- Selecionando dados distintos e agrupando os dados e gerando novos valores
+SELECT
+    Cidade
+FROM
+    [FundamentosTSQL].[Vendas].[Cliente]
+GO
+
+
+SELECT
+    DISTINCT Cidade
+FROM
+    [FundamentosTSQL].[Vendas].[Cliente]
+GO
+
+
+SELECT
+    Cidade
+  , COUNT(*) AS TotalClientes
+FROM
+    [FundamentosTSQL].[Vendas].[Cliente]
+GROUP BY
+    Cidade
+GO
+
+
+SELECT
+    Cidade
+  , COUNT(*) AS TotalClientes
+FROM
+    [FundamentosTSQL].[Vendas].[Cliente]
+GROUP BY
+    Cidade
+ORDER BY
+    TotalClientes DESC
+GO
+
+
+SELECT
+    Cidade
+  , COUNT(*) AS TotalClientes
+FROM
+    [FundamentosTSQL].[Vendas].[Cliente]
+GROUP BY
+    Cidade
+HAVING
+    COUNT(*) >= 2
+ORDER BY
+    TotalClientes DESC
+GO
